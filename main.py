9c373400d3d9d41221ba0ac9ea100940bb83d16a
Romane G <romane71193@gmail.com>
2019-04-01 22:05:04 -0400

    firstmissingpositive

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 
+def firstMissingPositive(nums):#Our original array 
+  
+    m = max(nums) #Store max number in Array
+    if m < 1: 
+          
+      #If all numbers in array are negative,just return 1 
+        return 1 
+    if len(nums) == 1: 
+          
+        #In case it contains one element 
+        return 2 if nums[0] == 1 else 1     
+    l = [0] * m 
+    for i in range(len(nums)): 
+        if nums[i] > 0: 
+            if l[nums[i] - 1] != 1: 
+                  
+                #Changing the value at list index
+                l[nums[i] - 1] = 1 
+    for i in range(len(l)): 
+          
+  #Encountering first 0, i.e, the element with least value 
+        if l[i] == 0:  
+            return i+1
+      #In case all values are filled between 1 and the max 
+    return i+2 
 
-print("hi")
+
+print(firstMissingPositive([0,1,2]))
